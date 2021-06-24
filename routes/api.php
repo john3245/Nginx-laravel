700e0e8926df04bcdd0ad46d4bfa090ac05b9337
john3245 <52746923+john3245@users.noreply.github.com>
2021-06-24 03:45:48 -0500

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -17,7 +17,9 @@ use Illuminate\Support\Facades\Route;
 /*
 prueba
 */
-
+/*
+prueba
+*/
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
